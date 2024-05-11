1234d53102c6c9e3a77dcd5d5d975feba95d403c
Thinh <thinh1198@hotmail.com>
2024-05-11 23:22:28 +0100

    styling

--- src/pages/main/home.jsx
+++ src/pages/main/home.jsx
@@ -1,10 +1,43 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 
 function Home() {
+
+    function getWindowDimensions() {
+        const { innerWidth: width, innerHeight: height } = window;
+
+        return {
+            width, height
+        }
+    }
+    
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions())
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+
+    document.documentElement.style.setProperty(
+        '--hero-height', windowDimensions.height / 100 + 'px',
+    )
 
     return (
         <>
             <section className='home__hero'>
-
+                <Link to={''} className='home__hero-link'>
+                    <div className='home__hero-wrapper'>
+                        <div className='hero__image'>
+                            <img src="https://placehold.co/1440x1440/png" alt=""/>
+                        </div>
+                    </div>
+                </Link>
             </section>
         </>
     )
