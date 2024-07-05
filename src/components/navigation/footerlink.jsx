b03dd41ad08a8571182c1ff98754110786bb624e
Thinh <thinh1198@hotmail.com>
2024-07-05 21:37:01 +0100

    add logo to footer social link

--- src/components/navigation/footerlink.jsx
+++ src/components/navigation/footerlink.jsx
@@ -1,4 +1,5 @@
-import { NavRoute } from '../route/navroute'
+import { NavRoute } from '../route/navroute';
+import { Instagram, Facebook, Pinterest, YouTube } from '@mui/icons-material/';
 
 export const FooterNav = () => {
 
@@ -6,42 +7,86 @@ export const FooterNav = () => {
 
         const sections = {
             Navigate: [
-                'Shop',
-                'About us',
-                'Blog',
-                'Career',
+                {
+                    label: 'Shop',
+                },
+                {
+                    label: 'About us',
+                },
+                {
+                    label: 'Blog',
+                },
+                {
+                    label: 'Career',
+                },
             ],
             Social: [
-                'Instagram',
-                'Pinterest',
-                'Facebook',
-                'Tiktok',
-                'Youtube',
+                {
+                    label: 'Instagram',
+                    icon: <Instagram className='footer__nav-icon'/>,
+                },
+                {
+                    label: 'Pinterest',
+                    icon: <Pinterest className='footer__nav-icon'/>,
+                },
+                {
+                    label: 'Facebook',
+                    icon: <Facebook className='footer__nav-icon'/>,
+                },
+                {
+                    label: 'TikTok',
+                    icon: <Facebook className='footer__nav-icon'/>,
+                },
+                {
+                    label: 'Youtube',
+                    icon: <YouTube className='footer__nav-icon'/>,
+                },
             ],
             Support: [
-                'Help & FAQ',
-                'Contact',
-                'Return & Exchange',
-                'Shipping Information',
-                'Privacy',
+                {
+                    label: 'Help & FAQ',
+                },
+                {
+                    label: 'Contact',
+                },
+                {
+                    label: 'Return & Exchange',
+                },
+                {
+                    label: 'Shipping Information',
+                },
+                {
+                    label: 'Privacy',
+                },
             ],
         }
 
-        return Object.entries(sections).map(([section, links], i) => {
+        return Object.entries(sections).map(([section, sub], i) => {
             return (
                 <div className='footer__nav-links' key={i}>
-                    <h4 className='footer__nav-header'>
+                    <h4 className='footer__nav-header'> 
                         { section }
                     </h4>
                     <ul className='footer__nav-list'>
-                        { links.map((link, i) => {
-                            return (
-                                <li className='footer__nav-item' key={i}>
-                                    <NavRoute to='/' className={'footer__nav-link'}>
-                                        { link }
-                                    </NavRoute>
-                                </li>
-                            )
+                        { sub.map((link, i) => {
+                            if ( link.icon ) {
+                                return (
+                                    <li className='footer__nav-item' key={i}>
+                                        <NavRoute to='/' className='footer__nav-link'>
+                                            { link.icon }
+                                            <span className='footer__nav-label'>{ link.label }</span>
+                                        </NavRoute>
+                                    </li>
+                                )
+                            } else {
+                                return (
+                                    <li className='footer__nav-item' key={i}>
+                                        <NavRoute to='/' className={'footer__nav-link'}>
+                                            { link.label }
+                                        </NavRoute>
+                                    </li>
+                                )
+                            }
                         })}
                     </ul>
                 </div>
