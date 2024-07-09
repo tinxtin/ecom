26bde217e4fa362be5cafbfbd2fdcd3656c459b4
Thinh <thinh1198@hotmail.com>
2024-07-09 19:39:56 +0100

    style dropdown carousel

--- src/components/navigation/navbaritem.jsx
+++ src/components/navigation/navbaritem.jsx
@@ -5,11 +5,9 @@ import { DropDownItem } from './dropdownitem';
 export const NavItem = ({ label, href, children}) => {
     return (
         <li className='nav__item'>
-            <div className="nav__item-content">
-                <NavRoute to={href} className={({ isActive }) => isActive ? 'nav__item-link active': 'nav__item-link'}>
-                    { label }
-                </NavRoute>
-            </div>
+            <NavRoute to={href} className={({ isActive }) => isActive ? 'nav__item-link active': 'nav__item-link'}>
+                { label }
+            </NavRoute>
             {children && 
                 <div className='nav__item-dropdown'>
                         <DropDownItem sub={children}/>
